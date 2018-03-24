0af3cb8a6b7004796cc66c8135f1975b2d15fdb0
Mikhail Kuzmin <m.kuzmin@darkleaf.ru>
2018-03-24 21:54:25 +0400

    fix cider

--- publicator/cider/src/darkleaf/cider.clj
+++ publicator/cider/src/darkleaf/cider.clj
@@ -1,18 +1,6 @@
 (ns darkleaf.cider
   (:require [clojure.tools.nrepl.server :as nrepl-server]))
 
-(defn- stop [])
-(defn- start [])
-
-(defn- inject-start-stop []
-  (let [user (find-ns 'user)]
-    (when-not (ns-resolve user 'stop)
-      (intern 'user 'stop stop))
-    (when-not (ns-resolve user 'start)
-      (intern 'user 'start start))))
-
-(inject-start-stop)
-
 (defn- nrepl-handler []
   (require 'cider.nrepl)
   (ns-resolve 'cider.nrepl 'cider-nrepl-handler))
