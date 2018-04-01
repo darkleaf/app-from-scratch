09961e420097bf8d6e6ea36ef49af7d0077e73dd
Mikhail Kuzmin <m.kuzmin@darkleaf.ru>
2018-04-01 18:02:33 +0400

    post queries refactoring

--- publicator/core/src/publicator/use_cases/test/fakes/post_queries.clj
+++ publicator/core/src/publicator/use_cases/test/fakes/post_queries.clj
@@ -5,38 +5,34 @@
    [publicator.domain.aggregates.user :as user]
    [publicator.domain.services.user-posts :as user-posts]))
 
+(defn- author-for-post [db post]
+  (->> @db
+       (vals)
+       (filter user/user?)
+       (filter #(user-posts/author? % post))
+       (first)))
+
+(defn- assoc-user-fields [post user]
+  (assoc post
+         ::user/id (:id user)
+         ::user/full-name (:full-name user)))
+
 (deftype GetList [db]
   post-q/GetList
   (-get-list [_]
     (->> @db
          (vals)
-         (filter user/user?)
-         (mapcat (fn [user]
-                   (->> @db
-                        (vals)
-                        (filter post/post?)
-                        (map #(assoc %
-                                     ::user/id (:id user)
-                                     ::user/full-name (:full-name user)))))))))
+         (filter post/post?)
+         (map #(when-some [author (author-for-post db %)]
+                 (assoc-user-fields % author)))
+         (remove nil?))))
 
 (deftype GetById [db]
   post-q/GetById
   (-get-by-id [_ id]
     (when-some [post (get @db id)]
-      (prn (->> @db
-                (vals)
-                (filter user/user?)
-                (filter #(user-posts/author? % post))))
-
-
-      (let [user (->> @db
-                      (vals)
-                      (filter user/user?)
-                      (filter #(user-posts/author? % post))
-                      (first))]
-        (assoc post
-               ::user/id (:id user)
-               ::user/full-name (:full-name user))))))
+      (when-some [author (author-for-post db post)]
+        (assoc-user-fields post author)))))
 
 (defn binding-map [db]
   {#'post-q/*get-list* (->GetList db)
