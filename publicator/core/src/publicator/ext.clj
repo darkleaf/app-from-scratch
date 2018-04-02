8061beb1b7fcd91a12492212478e270b67833ab1
Mikhail Kuzmin <m.kuzmin@darkleaf.ru>
2018-04-03 00:37:30 +0400

    speedup tests

--- publicator/core/src/publicator/ext.clj
+++ publicator/core/src/publicator/ext.clj
@@ -4,12 +4,15 @@
 
 ;; https://groups.google.com/forum/#!topic/clojure/fti0eJdPQJ8
 (defmacro only-keys [& {:keys [req req-un opt opt-un] :as args}]
-  `(s/merge (s/keys ~@(apply concat (vec args)))
-            (s/map-of ~(set (concat req
-                                    (map (comp keyword name) req-un)
-                                    opt
-                                    (map (comp keyword name) opt-un)))
-                      any?)))
+  (let [keys-spec `(s/keys ~@(apply concat (vec args)))]
+    `(s/with-gen
+       (s/merge ~keys-spec
+                (s/map-of ~(set (concat req
+                                        (map (comp keyword name) req-un)
+                                        opt
+                                        (map (comp keyword name) opt-un)))
+                          any?))
+       #(s/gen ~keys-spec))))
 
 (defn in? [coll elm]
   (boolean (some #(= elm %) coll)))
