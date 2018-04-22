1f78f727fe73b067033c71014302b8a79a457132
Mikhail Kuzmin <m.kuzmin@darkleaf.ru>
2018-04-22 21:07:55 +0400

    root

--- publicator/web/src/publicator/web/controllers/pages/root.clj
+++ publicator/web/src/publicator/web/controllers/pages/root.clj
@@ -1,12 +1,10 @@
 (ns publicator.web.controllers.pages.root
   (:require
    [ring.util.http-response :as http-response]
-   [publicator.web.template :as template]))
+   [publicator.web.controllers.base :as base]))
 
 (defn show [req]
-  (-> (template/render "pages/root" {})
-      (http-response/ok)
-      (http-response/content-type "text/html")))
+  (base/render "pages/root" {}))
 
 (def routes
   #{[:get "/" #'show :pages/root]})
